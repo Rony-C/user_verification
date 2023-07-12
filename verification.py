ac0743d9398b5067b1232dd86f72bb351a88904a
Ronan <53212845+Rony-C@users.noreply.github.com>
2023-07-12 10:52:43 +0100

    Update verification.py
    Added comments to each section
    Updated code to only open in Chrome

--- verification.py
+++ verification.py
@@ -1,21 +1,30 @@
 import webbrowser
 import argparse
 
+#Parse CLI arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', '-i', required=True, help='Input file of IDs to check Verification.')
-
 args = parser.parse_args()
 file = args.input
 
+#Open program in Chrome instead of default browser
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+#Stores URLs to be checked
 urlList = []
 
+#Open file added via CLI
 with open(file, 'r') as f:
     lines = f.readlines()
     for line in lines:
+        #Gets ID digits only
         line = line.strip('\n')
+        #Creates full URL and stores it in list
         url = f'https://test.com/login/{line}/check-id'
         urlList.append(url)      
+#Loops through URL List and opens it in Chrome
 for url in urlList:
-        webbrowser.open_new_tab(url) 
+        webbrowser.get(chrome_path).open_new_tab(url) 
+#Prints how many IDs have been checked
 print(f'{len(urlList)} IDs checked')
         
