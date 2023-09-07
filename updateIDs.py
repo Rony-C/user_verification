df0a78c8bc328a5eb66eb082e2beb7e316ae07ea
Ronan <53212845+Rony-C@users.noreply.github.com>
2023-09-07 14:51:42 +0100

    Update updateIDs.py
    Generalising content

--- updateIDs.py
+++ updateIDs.py
@@ -1,5 +1,5 @@
 import pyperclip
 
 #Create file and write to it
-with open('/Users/rony/vs_code/python/work_files/ids.txt', 'w+') as f:
+with open('directory_path/ids.txt', 'w+') as f:
     f.write(pyperclip.paste())
